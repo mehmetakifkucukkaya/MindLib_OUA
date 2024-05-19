e1677e476db5377fa2f172944ebef2824b411654
mehmetakifkucukkaya <mehmet.akif.20021@gmail.com>
2024-05-19 13:20:43 +0300

    Added BookCard widget

--- lib/views/book_summaries_page.dart
+++ lib/views/book_summaries_page.dart
@@ -1,12 +1,28 @@
 import 'package:flutter/material.dart';
 
+import '../models/book_model.dart';
+import '../widgets/book_card.dart';
+
 class BookSummariesPage extends StatelessWidget {
   const BookSummariesPage({super.key});
-
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('Welcome to the Book Summaries Page!'),
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: GridView.builder(
+          itemCount: books.length,
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 3,
+            childAspectRatio: 0.55,
+            crossAxisSpacing: 8.0,
+            mainAxisSpacing: 8.0,
+          ),
+          itemBuilder: (context, index) {
+            return BookCard(book: books[index]);
+          },
+        ),
+      ),
     );
   }
 }
---
+++ lib/widgets/book_card.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+import '../models/book_model.dart';
+
+class BookCard extends StatelessWidget {
+  final Book book;
+
+  const BookCard({super.key, required this.book});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      color: Colors.grey[300],
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Expanded(
+            child: Image.network(
+              book.imageUrl,
+              fit: BoxFit.cover,
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text(
+              book.title,
+              style: const TextStyle(
+                fontSize: 15,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 8.0),
+            child: Text(book.author),
+          ),
+        ],
+      ),
+    );
+  }
+}
