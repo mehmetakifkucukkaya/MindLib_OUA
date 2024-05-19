437ac76e30eadd9e9fe616620e8bf21d7dfd9656
mehmetakifkucukkaya <mehmet.akif.20021@gmail.com>
2024-05-19 13:45:39 +0300

    updated BookModel

--- lib/models/book_model.dart
+++ lib/models/book_model.dart
@@ -2,65 +2,131 @@ class Book {
   final String title;
   final String author;
   final String imageUrl;
+  final String summary;
 
   Book({
     required this.title,
     required this.author,
     required this.imageUrl,
+    required this.summary,
   });
 }
 
 final List<Book> books = [
   Book(
-    title: 'Rich Dad Poor Dad',
-    author: 'Robert T. Kiyosaki',
-    imageUrl:
-        'https://m.media-amazon.com/images/I/81BE7eeKzAL._SY425_.jpg',
-  ),
+      title: 'Zengin Baba Yoksul Baba',
+      author: 'Robert T. Kiyosaki',
+      imageUrl:
+          'https://m.media-amazon.com/images/I/41QyIIWvN0L._SY445_SX342_.jpg',
+      summary: ''),
   Book(
-    title: 'The Alchemist',
-    author: 'Paulo Coelho',
-    imageUrl:
-        'https://m.media-amazon.com/images/I/71pJIgY8ZuL._SY425_.jpg',
-  ),
+      title: 'Simyacı',
+      author: 'Paulo Coelho',
+      imageUrl:
+          'https://m.media-amazon.com/images/I/41N1PqT+HPL._SX342_SY445_.jpg',
+      summary: ''),
   Book(
-    title: 'The Fault in Our Stars',
-    author: 'John Green',
+    title: 'Tutunamayanlar',
+    author: 'Oğuz Atay',
     imageUrl: 'https://via.placeholder.com/150',
+    summary: '',
   ),
   Book(
-    title: 'Fangirl',
-    author: 'Rainbow Rowell',
+    title: 'Kürk Mantolu Madonna',
+    author: 'Sabahattin Ali',
     imageUrl: 'https://via.placeholder.com/150',
+    summary: '',
   ),
   Book(
-    title: 'Divergent',
-    author: 'Veronica Roth',
+    title: 'Aşk',
+    author: 'Elif Şafak',
     imageUrl: 'https://via.placeholder.com/150',
+    summary: '',
   ),
   Book(
-    title: 'Fallen',
-    author: 'Lauren Kate',
+    title: 'Beyaz Gemi',
+    author: 'Cengiz Aytmatov',
     imageUrl: 'https://via.placeholder.com/150',
+    summary: '',
   ),
   Book(
-    title: 'Sylvia Day',
-    author: 'Sylvia Day',
+    title: 'İnce Memed',
+    author: 'Yaşar Kemal',
     imageUrl: 'https://via.placeholder.com/150',
+    summary: '',
   ),
   Book(
-    title: 'Darker',
-    author: 'E. L. James',
+    title: 'Yaban',
+    author: 'Yakup Kadri Karaosmanoğlu',
     imageUrl: 'https://via.placeholder.com/150',
+    summary: '',
   ),
   Book(
-    title: 'Gone Girl',
-    author: 'Gillian Flynn',
+    title: 'Anayurt Oteli',
+    author: 'Yusuf Atılgan',
     imageUrl: 'https://via.placeholder.com/150',
+    summary: '',
   ),
   Book(
-    title: 'Grey',
-    author: 'E. L. James',
+    title: 'Eylül',
+    author: 'Mehmet Rauf',
     imageUrl: 'https://via.placeholder.com/150',
+    summary: '',
   ),
+  Book(
+      title: 'Sergüzeşt',
+      author: 'Samipaşazade Sezai',
+      imageUrl: 'https://via.placeholder.com/150',
+      summary: ''),
+  Book(
+      title: 'Aşk-ı Memnu',
+      author: 'Halit Ziya Uşaklıgil',
+      imageUrl: 'https://via.placeholder.com/150',
+      summary: ''),
+  Book(
+    title: 'Çalıkuşu',
+    author: 'Reşat Nuri Güntekin',
+    imageUrl: 'https://via.placeholder.com/150',
+    summary: '',
+  ),
+  Book(
+      title: 'Atomik Alışkanlıklar',
+      author: 'James Clear',
+      imageUrl: 'https://via.placeholder.com/150',
+      summary: ''),
+  Book(
+      title: 'Düşlerin Kaynağı',
+      author: 'Sigmund Freud',
+      imageUrl: 'https://via.placeholder.com/150',
+      summary: ''),
+  Book(
+      title: 'Bilinçdışı',
+      author: 'Stefan Zweig',
+      imageUrl: 'https://via.placeholder.com/150',
+      summary: ''),
+  Book(
+      title: 'Amok Koşucusu',
+      author: 'Stefan Zweig',
+      imageUrl: 'https://via.placeholder.com/150',
+      summary: ''),
+  Book(
+      title: 'Deliliğe Övgü',
+      author: 'Erasmus',
+      imageUrl: 'https://via.placeholder.com/150',
+      summary: ''),
+  Book(
+      title: 'Suç ve Ceza',
+      author: 'Fyodor Dostoyevski',
+      imageUrl: 'https://via.placeholder.com/150',
+      summary: ''),
+  Book(
+      title: 'Karamazov Kardeşler',
+      author: 'Fyodor Dostoyevski',
+      imageUrl: 'https://via.placeholder.com/150',
+      summary: ''),
+  Book(
+      title: 'Yeraltından Notlar',
+      author: 'Fyodor Dostoyevski',
+      imageUrl: 'https://via.placeholder.com/150',
+      summary: ''),
 ];
